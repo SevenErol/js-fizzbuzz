350465f0c78403873bef86dfee4c88182e24b5d2
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-10 14:53:48 +0200

    block structure complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,23 @@ const multipleThree = "Fizz";
 
 const multipleFive = "Buzz";
 
+const container = document.querySelector(".container");
+
+
+
 for(let i = 1; i <= 100; i++) {
+
+    const boxElement = document.createElement("div");
+
+    boxElement.classList.add("box");
+
+    container.appendChild(boxElement);
+
+    const squareElement = document.createElement("div");
+
+    squareElement.classList.add("square");
+
+    boxElement.appendChild(squareElement);
 
     if (i % 3 === 0 && i % 5 === 0) {
 
