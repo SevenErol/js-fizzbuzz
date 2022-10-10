04910ac7e32287f675589d37d9c52b2510eb4b78
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-10 15:11:18 +0200

    bonus complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,21 +19,44 @@ for(let i = 1; i <= 100; i++) {
 
     squareElement.classList.add("square");
 
+    squareElement.classList.add("flex");
+
     boxElement.appendChild(squareElement);
+
+    const contentElement = document.createElement("div");
+
+    squareElement.appendChild(contentElement);
 
     if (i % 3 === 0 && i % 5 === 0) {
 
         console.log(`${multipleThree}${multipleFive}`);
 
+        squareElement.classList.add("fizz_buzz");
+
+        contentElement.innerHTML = `${multipleThree}${multipleFive}`;
+
     } else if (i % 5 === 0) {
 
         console.log(multipleFive);
 
+        squareElement.classList.add("buzz");
+
+        contentElement.innerHTML = `${multipleFive}`;
+
     } else if (i % 3 === 0) {
 
-        console.log(multipleThree)
+        console.log(multipleThree);
+
+        squareElement.classList.add("fizz");
+
+        contentElement.innerHTML = `${multipleThree}`;
 
     } else {
+
         console.log(i);
+
+        squareElement.classList.add("number");
+
+        contentElement.innerHTML = i;
     }
 }
